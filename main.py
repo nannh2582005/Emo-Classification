beda9bd3631df769feda27e50ba4e6f857eb63dc
qz4ng <hk9.ttquynhgiang@gmail.com>
2025-12-08 23:20:36 +0700

    test

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import pandas as pd
-import numpy as np
-import os
-import re
+import json
 
 from processor_layer.loader import DataLoader
 from processor_layer.processor import DataProcessor
@@ -14,6 +11,11 @@ def main():
     data = DataLoader(path)
     df = data.load_data()
     print(df.head(5))
-
+    with open(r'data/vietnamese-stopwords.txt', 'r', encoding='utf-8') as f:
+        stopwords = [line.strip() for line in f if line.strip()]
+    with open(r'data/emoji_dict.json','r', encoding='utf-8') as f:
+        emoji_dict = json.load(f)
+    with open(r'data/teencode_dict.json', 'r', encoding='utf-8') as f:
+        teencode_dict = json.load(f)
 if __name__ == "__main__":
     main()
