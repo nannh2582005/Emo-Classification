1ce8e801f7d707f3b518331652df778a7dfd9813
qz4ng <hk9.ttquynhgiang@gmail.com>
2025-12-08 23:09:17 +0700

    test

--- main.py
+++ main.py
@@ -3,18 +3,17 @@ import numpy as np
 import os
 import re
 
-from collections import Counter
-
-from DataPreprocessor.DataLoader import DataLoader
-from DataPreprocessor.DataLabeler import DataLabeling
-from DataPreprocessor.DataEncoder import DataEncoder
+from processor_layer.loader import DataLoader
+from processor_layer.processor import DataProcessor
+from feature_layer.tfidf import TFIDF
 
 def main():
     # đường dẫn dữ liệu (có thể thay đổi)
-    path = r"data/test_nor_811.xlsx" 
+    path = r"data/data.xlsx" 
 
-    data = DataLoader(file_path=path)
-    data.review_data()
+    data = DataLoader(path)
+    df = data.load_data()
+    print(df.head(5))
 
 if __name__ == "__main__":
     main()
