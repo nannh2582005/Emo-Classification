4c7bb1448a0cc94fb03a94cbc6b3f14174ad9ae7
qz4ng <hk9.ttquynhgiang@gmail.com>
2025-12-08 22:48:27 +0700

    cập nhật tham số

--- feature_layer/tfidf.py
+++ feature_layer/tfidf.py
@@ -1,21 +1,28 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from .base import DataFeature
+
 class TFIDF(DataFeature):
-    """vector hóa bằng phương pháp TFIDF
-    X: list những danh sách token, ví dụ: [["tôi", "yêu", "việt_nam"], ["hôm_nay", "trời", "đẹp"]]
-    sử dụng tham số để nhận đầu vào là danh sách đã tokenize"""
-    def __init__(self, texts: list[list[str]], max_features: int = 5000, ngram_range = (1,2)):
+    def __init__(self, texts: list[list[str]], 
+                 max_features: int = 10000, 
+                 ngram_range=(1, 2),
+                 min_df=3,
+                 max_df=0.95):
+        
         super().__init__(texts)
-        # tham số để nhận pretokenized list
+
         self.vectorizer = TfidfVectorizer(
             tokenizer=lambda x: x,
             preprocessor=lambda x: x,
             token_pattern=None,
-            max_features=max_features,  
-            ngram_range=ngram_range
+            max_features=max_features,
+            ngram_range=ngram_range,
+            min_df=min_df,
+            max_df=max_df,
+            sublinear_tf=True,
+            norm='l2'
         )
-    
-    def fit(self, X: list[list[str]]):
+
+    def fit(self, X):
         self.vectorizer.fit(X)
 
     def transform(self, X):
